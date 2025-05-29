9cbddd91caefcca6398cb1da9ea8f48ae0a8c53a
Shema Cedric Landry <88650841+Shema21@users.noreply.github.com>
2025-05-29 13:48:14 +0100

    Update fixtures.py

--- 0x03-Unittests_and_integration_tests/fixtures.py
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+org_payload = {
+    "login": "google",
+    "id": 123,
+    "repos_url": "https://api.github.com/orgs/google/repos"
+}
+
+repos_payload = [
+    {"name": "repo1", "license": {"key": "apache-2.0"}},
+    {"name": "repo2", "license": {"key": "other"}},
+    {"name": "repo3", "license": {"key": "apache-2.0"}},
+]
+
+expected_repos = ["repo1", "repo2", "repo3"]
+apache2_repos = ["repo1", "repo3"]
+
+
 TEST_PAYLOAD = [
   (
     {"repos_url": "https://api.github.com/orgs/google/repos"},
