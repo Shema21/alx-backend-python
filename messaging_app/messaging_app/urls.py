4367b6110908c7c53208ae1e6901be5226c73ab5
Shema Cedric Landry <88650841+Shema21@users.noreply.github.com>
2025-06-04 08:54:29 +0100

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from chats.views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet)
+router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')), 
 ]
